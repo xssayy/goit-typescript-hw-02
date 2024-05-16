7f0111d0ff63ee406629037fa3819470eed0d6f7
Mykyta Kudlai <nikiatanik@gmail.com>
2024-05-16 14:01:41 +0200

    fix get data typisation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,18 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { GalleryItem } from "../components/ImageCard/ImageCard.types";
 
 axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
-export const getData = async (query: string, page: number) => {
-  const response = await axios.get("", {
+interface UnsplashResponse {
+  results: GalleryItem[];
+  total: number;
+  total_pages: number;
+}
+
+export const getData = async (
+  query: string,
+  page: number
+): Promise<UnsplashResponse> => {
+  const response: AxiosResponse<UnsplashResponse> = await axios.get("", {
     params: {
       client_id: "RYfnsD_OSWqdJOknKnh_kbmhW4zYn8qLrrzs1hxPv5o",
       query,
